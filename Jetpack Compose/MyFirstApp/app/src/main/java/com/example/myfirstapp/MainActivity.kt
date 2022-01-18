4ff7b49b8d98d58da18b826d0114879f44d3a009
mursalin117 <mursalinchowdhury117@gmail.com>
2022-01-18 21:49:30 +0600

    First app using Jetpack Compose

---
+++ Jetpack Compose/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myfirstapp.ui.theme.MyFirstAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting(name = "world!")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Greeting("world")
+}
