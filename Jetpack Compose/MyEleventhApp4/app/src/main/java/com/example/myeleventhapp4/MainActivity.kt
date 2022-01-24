8d7de2de5dc95265de54a3475cbf873f11ab0e5b
mursalin117 <mursalinchowdhury117@gmail.com>
2022-01-24 16:51:40 +0600

    increasing shape using animation 4

--- Jetpack Compose/MyEleventhApp4/app/src/main/java/com/example/myeleventhapp4/MainActivity.kt
+++ Jetpack Compose/MyEleventhApp4/app/src/main/java/com/example/myeleventhapp4/MainActivity.kt
@@ -1,2 +1,56 @@
 package com.example.myeleventhapp4
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var sizestate by remember {
+                mutableStateOf(200.dp)
+            }
+            val size by animateDpAsState(
+                targetValue = sizestate,
+                tween(
+                    durationMillis = 1000
+                )
+            )
+            val infiniteTransition = rememberInfiniteTransition()
+            val color by infiniteTransition.animateColor(
+                initialValue = Color.Red,
+                targetValue = Color.Green,
+                animationSpec = infiniteRepeatable(
+                    tween(durationMillis = 2000),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+            Box(
+                modifier = Modifier
+                    .size(size)
+                    .background(color),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(onClick = {
+                    sizestate += 50.dp
+                }) {
+                    Text(text = "Increased Size")
+                }
+            }
+        }
+    }
+}
