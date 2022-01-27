7f2d7680d551b8f9afb85b0ef24ba32859aa14f7
mursalin117 <mursalinchowdhury117@gmail.com>
2022-01-28 00:10:36 +0600

    meditation ui created

---
+++ Jetpack Compose/MeditationUI/app/src/main/java/com/example/meditationui/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.meditationui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.meditationui.ui.HomeScreen
+//import com.example.meditationui.ui.HomeScreen2
+import com.example.meditationui.ui.theme.MeditationUITheme
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditationUITheme {
+                HomeScreen()
+            }
+        }
+    }
+}
